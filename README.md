f5da17e9dbd45032a5abf6442895530b0743e971
GlauberMargem <126527963+GlauberMargem@users.noreply.github.com>
2023-06-11 13:36:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ghostemane_fan_site
+ 
---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+  $('.galery').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
+  $('.galery2').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
+  $('.galery3').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{enabled:true}
+    // other options
+  });
